3070ab1346be0866e3225af8167938342ae4bc10
WalterPlinge <22519813+WalterPlinge@users.noreply.github.com>
2021-08-14 03:33:55 +0100

    Moved away from optional fixed width types

--- code/main.c
+++ code/main.c
@@ -16,19 +16,19 @@
 
 
 
-#define BETWEEN( x, a, b ) ( (a) < (x) && (x) < (b) )
+#define BETWEEN( x, a, b ) ( (a) < (x) and (x) < (b) )
 
 
 
-int64_t  typedef s8;
-int32_t  typedef s4;
-int16_t  typedef s2;
-int8_t   typedef s1;
+int_least64_t  typedef s8;
+int_least32_t  typedef s4;
+int_least16_t  typedef s2;
+int_least8_t   typedef s1;
 
-uint64_t typedef u8;
-uint32_t typedef u4;
-uint16_t typedef u2;
-uint8_t  typedef u1;
+uint_least64_t typedef u8;
+uint_least32_t typedef u4;
+uint_least16_t typedef u2;
+uint_least8_t  typedef u1;
 
 u1       typedef byte;
 

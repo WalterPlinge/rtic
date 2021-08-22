86ea7a299996013736e592daf5931ac16dcf4917
WalterPlinge <22519813+WalterPlinge@users.noreply.github.com>
2021-08-22 19:41:43 +0100

    Change make flag to build release, and debug by default

--- make.bat
+++ make.bat
@@ -7,14 +7,16 @@ set Output=-Fe:%AppName% -Fa%AppName% -FAasu
 set Debug=-fp:precise -Od
 set Release=-fp:fast -O2 -openmp
 
-set CompilerFlags=%Output% %Debug% -analyze- -FC -MTd -std:c17 -TC -utf-8 -W3 -Zi
-set LinkerFlags=-subsystem:console
+set CompilerFlags=%Output% -analyze- -FC -MTd -std:c17 -TC -utf-8 -W3 -Zi
+set LinkerFlags=-link -libpath:../libs/ -subsystem:console
 
 if not exist build mkdir build
 pushd build
 
-call cl ../code/main.c %CompilerFlags% -link -libpath:../libs/ %LinkerFlags%
-
-if "%~1" == "-r" call %AppName%.exe
+if "%~1" == "-r" (
+	call cl ../code/main.c %Release% %CompilerFlags% %LinkerFlags%
+) else (
+	call cl ../code/main.c %Debug%   %CompilerFlags% %LinkerFlags%
+)
 
 popd

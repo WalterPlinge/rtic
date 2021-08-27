4836c1b02f2ee7a82ed5b265afb0c8b1c8cd5b44
WalterPlinge <22519813+WalterPlinge@users.noreply.github.com>
2021-08-27 20:00:10 +0100

    Update make.bat

--- make.bat
+++ make.bat
@@ -2,11 +2,13 @@
 setlocal
 
 set AppName=rtic
+set CodeFile=../code/main.c
+set Compiler=cl
 
-set Output=-Fe:%AppName% -Fa%AppName% -FAasu
 set Debug=-fp:precise -Od
 set Release=-fp:fast -O2 -openmp
 
+set Output=-Fe:%AppName% -Fa%AppName% -FAasu
 set CompilerFlags=%Output% -analyze- -FC -MTd -std:c17 -TC -utf-8 -W3 -Zi
 set LinkerFlags=-link -libpath:../libs/ -subsystem:console
 
@@ -14,9 +16,9 @@ if not exist build mkdir build
 pushd build
 
 if "%~1" == "-r" (
-	call cl ../code/main.c %Release% %CompilerFlags% %LinkerFlags%
+	call %Compiler% %CodeFile% %Release% %CompilerFlags% %LinkerFlags%
 ) else (
-	call cl ../code/main.c %Debug%   %CompilerFlags% %LinkerFlags%
+	call %Compiler% %CodeFile% %Debug%   %CompilerFlags% %LinkerFlags%
 )
 
 popd
